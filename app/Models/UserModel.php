6934b97345698320be2d6d673bad552e4b5c7cd7
VolodymyrShkykavyi <volodymyr.shkykavyi@gmail.com>
2018-07-04 18:53:17 +0300

    primitive authorization

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -12,4 +12,12 @@ class UserModel extends Model
 		return $this->db->query('SELECT * FROM `users`')->fetchAll(PDO::FETCH_ASSOC);
 	}
 
+	public function auth($login, $password)
+	{
+		//TODO: hash psw
+		$stmt = $this->db->prepare('SELECT * FROM `users` WHERE `login` = ? AND `password` = ?');
+		$stmt->execute([$login, $password]);
+
+		return $stmt->fetch(PDO::FETCH_ASSOC);
+	}
 }
