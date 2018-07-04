107c5597115a4538f1cbb42b33f3dd14e530f7fb
VolodymyrShkykavyi <volodymyr.shkykavyi@gmail.com>
2018-07-04 16:11:54 +0300

    group for unauthorized users

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,22 @@
 <?php
 
 use App\Controllers\UserController;
+use App\Middleware\RedirectIfAuthorized;
 use App\Middleware\RedirectIfUnauthorized;
-use \App\Library\DbInit;
+use App\Library\DbInit;
 
 $app->get('/install', DbInit::class);
 
 $app->get('/', UserController::class . ':secret');
 $app->map(['GET', 'POST'],'/{id:[0-9]+}', UserController::class . ':home')
 	->setName('login');
+
+
+//pages only for unauthorized users
+$app->group('', function(){
+
+})->add(new RedirectIfAuthorized($container['router']));
+
 
 //pages only for authorized users
 $app->group('', function (){
