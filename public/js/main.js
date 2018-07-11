5a40f62700331a3a60a8db27463634cd41059d7c
VolodymyrShkykavyi <volodymyr.shkykavyi@gmail.com>
2018-07-11 16:39:04 +0300

    add changing password

--- public/js/main.js
+++ public/js/main.js
@@ -16,10 +16,47 @@ function change_status($el) {
 	});
 }
 
+
+//show menu content
 $('#accordion a.clickable').click(function(e){
 	e.preventDefault();
 	let id = $(this).prop('id');
 
 	$(".settings .ui-block").hide();
 	$(".settings .ui-block." + id).show();
+});
+
+//change password
+$("#form_change_password button").click(function(){
+	let data = $("#form_change_password").serializeArray().reduce(function(obj, item) {
+    	obj[item.name] = item.value;
+   		return obj;
+	}, {});
+
+	if (data.new_psw !== data.confirm){
+		alert('don\'t match');
+		return false;
+	}
+	if (data.new_psw.length < 6){
+		alert('too small');
+		return false;
+	}
+
+	if (!data.new_psw.match(/[A-Za-z].*[0-9]|[0-9].*[A-Za-z]/g)){
+		alert('numbers and letters');
+		return false;
+	}
+
+	$.ajax({
+		type: 'POST',
+		url: '/password/change',
+		data: (data),
+		success: function (result) {
+			if (result == 'true'){
+				alert('psw changed');
+			} else {
+				alert('smt wrong');
+			}
+		}
+	});
 });
