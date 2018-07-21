813d99ab5a94a872a80d9d4c3ccdae598660acd2
Vitalii BONDARENKO <vibondar@e1r3p1.unit.ua>
2018-07-21 13:10:43 +0300

    add IsOnline beta

--- app/Controllers/Chat.php
+++ app/Controllers/Chat.php
@@ -4,45 +4,66 @@ namespace App\Controllers;
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
-class Chat implements MessageComponentInterface {
-    protected $clients;
+class Client
+{
+	private $_id = 0;
+	private $_conn = NULL;
 
-    public function __construct(){
-        $this->clients = new \SplObjectStorage;
-    }
+	public function __construct($id, $conn){
+		$this->_id = $id;
+		$this->_conn = $conn;
+	}
+	public function getId(){
+		return $this->_id;
+	}
+	public function getConn(){
+		return $this->_conn;
+	}
+}
 
-    public function onOpen(ConnectionInterface $conn) {
-        // Store the new connection to send messages to later
-        $this->clients->attach($conn);
-       $querystring = $conn->httpRequest->getUri()->getQuery();
-       parse_str($querystring,$queryarray);
-        echo "New connection! ({$conn->resourceId})\n";
-         var_dump($queryarray);
-    }
+class Chat extends Controller implements MessageComponentInterface {
+	protected $clients;
 
-    public function onMessage(ConnectionInterface $from, $msg)
-    {
-        $numRecv = count($this->clients) - 1;
-        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
-            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+	public function __construct(){
+		$this->clients = new \SplObjectStorage;
+	}
+	public function onOpen(ConnectionInterface $conn) {
+		// Store the new connection to send messages to later
+		$querystring = $conn->httpRequest->getUri()->getQuery();
+		parse_str($querystring,$queryarray);
+		$client = new Client($queryarray["id"], $conn);
+		$this->model->updateIsOnline($this->$queryarray["id"], true);
+		$this->clients->attach($client);
+		echo "New connection! ({$conn->resourceId})\n";
+	}
+
+	public function onMessage(ConnectionInterface $from, $msg)
+	{
+		$numRecv = count($this->clients) - 1;
+		echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+			, $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
 
 
-        foreach ($this->clients as $client) {
-            $client->send($msg);
-        }
-    }
+		foreach ($this->clients as $client) {
+				$client->getConn()->send($msg . "target id = {$client->getId()}");
+		}
+	}
 
-    public function onClose(ConnectionInterface $conn)
-    {
-        // The connection is closed, remove it, as we can no longer send it messages
-        $this->clients->detach($conn);
+	public function onClose(ConnectionInterface $conn)
+	{
+		// The connection is closed, remove it, as we can no longer send it messages
+		foreach ($this->clients as &$client) {
+			if ($client->getConn() === $conn){
+				$this->model->updateIsOnline($this->clients->getId(), false);
+				echo "Connection {$conn->resourceId} has disconnected\n";
+				$this->clients->detach($client);
+			}
+		}
+	}
 
-        echo "Connection {$conn->resourceId} has disconnected\n";
-    }
+	public function onError(ConnectionInterface $conn, \Exception $e) {
+		echo "An error has occurred: {$e->getMessage()}\n";
 
-    public function onError(ConnectionInterface $conn, \Exception $e) {
-        echo "An error has occurred: {$e->getMessage()}\n";
-
-        $conn->close();
-    }
+		$conn->close();
+	}
 }
