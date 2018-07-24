8f556cf50965070996a5a6ac2bedf569fc215258
VolodymyrShkykavyi <volodymyr.shkykavyi@gmail.com>
2018-07-24 15:37:29 +0300

    friends page

--- app/Models/ChatModel.php
+++ app/Models/ChatModel.php
@@ -14,7 +14,7 @@ class ChatModel{
 		$settings = include_once __DIR__ . "/../../config/slim.settings.php";
 		$conf = $settings['settings']['db'];
 		try{
-			$this->db = Mysql::create($conf['host'], $conf['user'], $conf['pass'], 3306, "/Users/vibondar/MAMP/mysql/tmp/mysql.sock");
+			$this->db = Mysql::create($conf['host'], $conf['user'], $conf['pass'], $conf['port'], $conf['socket']);
 		$this->db->setDatabaseName($conf['dbname']);
 		} catch (\Exception $ex){
 			echo "Can't connect to DataBase " . $ex->getMessage();
