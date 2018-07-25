a0ea4a035738969593075c487c8919d30f7a706a
VolodymyrShkykavyi <volodymyr.shkykavyi@gmail.com>
2018-07-25 15:55:39 +0300

    check for email

---
+++ app/Models/ApiModel.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Models;
+
+class ApiModel extends Model
+{
+
+	public function checkEmail($userId, $email)
+	{
+		$res = $this->db->query("SELECT * FROM `users` WHERE email='?s' AND NOT id = ?i", $email, $userId);
+
+		return empty($res->fetch_assoc());
+	}
+
+}
--- public/js/settings.js
+++ public/js/settings.js
@@ -1,23 +1,22 @@
-function showError(elem, msg)
-{
+let errors = 0;
+let wrongEmail = 0;
+
+function showError(elem, msg) {
 	$(elem).next().css("color", "red");
 	$(elem).next()[0].innerText = msg;
 }
 
-function clearErrors()
-{
+function clearErrors() {
 	$("span.material-input").html('');
 }
 
-function getDateObj(str)
-{
+function getDateObj(str) {
 	let dateParts = str.split("/");
 
 	return new Date(dateParts[2], dateParts[1] - 1, dateParts[0]); // month is 0-based
 }
 
-function getAge(pickerStr)
-{
+function getAge(pickerStr) {
 	let birthDate = getDateObj(pickerStr);
 	let today = new Date();
 
@@ -30,10 +29,37 @@ function getAge(pickerStr)
 	return age;
 }
 
+function checkEmail() {
+	let data = {};
+	let input = $("#personal_information_form input[name='email']")[0];
+	data.email = input.value;
+	$.ajax({
+		type: 'POST',
+		url: '/check/email',
+		data: data,
+		async: false,
+		success: function (response) {
+				if (response === 'true') {
+					$(input).css("border-color", "green");
+					showError(input, "");
+					wrongEmail = 0;
+				} else if (response === 'false') {
+					//already exists
+					$(input).css("border-color", "red");
+					wrongEmail = 1;
+					showError(input, "Email already exists");
+				}
+			}
+	});
+
+}
+
+$("#personal_information_form input[name='email']").keyup(checkEmail);
+
+
 $("#personal_information_form button").click(function (ev) {
 	let data = {};
 	let form = $("#personal_information_form")[0];
-	let errors = 0;
 	let age = getAge(form.datetimepicker.value);
 
 	ev.preventDefault();
@@ -43,46 +69,55 @@ $("#personal_information_form button").click(function (ev) {
 	data.description = $(form.description)[0].value;
 	data.sexual_preferenses = $(form.sex_preferences).val();
 	data.lat = $('#lat').text();
-	data.lng= $('#lng').text();
+	data.lng = $('#lng').text();
 
-	if (form.firstName.value.length > 30){
+	if (form.firstName.value.length > 30) {
 		showError(form.firstName, "Too long value. Max length 30 chars");
 		errors++;
 	} else {
 		data.firstName = form.firstName.value;
 	}
 
-	if (form.lastName.value.length > 30){
+	if (form.lastName.value.length > 30) {
 		showError(form.lastName, "Too long value. Max length 30 chars");
 		errors++;
 	} else {
 		data.lastName = form.lastName.value;
 	}
 
-	if (form.email.value.length > 150){
+	if (form.email.value.length > 150) {
 		showError(form.email, "Too long value. Max length 150 chars");
 		errors++;
 	} else {
-		data.email = form.email.value;
+		checkEmail();
 	}
 
-	if (form.custom_status.value.length > 50){
+	if (form.custom_status.value.length > 50) {
 		showError(form.custom_status, "Too long value. Max length 50 chars");
 		errors++;
 	} else {
 		data.custom_status = form.custom_status.value;
 	}
 
-	if (age < 18){
+	if (age < 18) {
 		showError(form.datetimepicker, "You are too young.");
 		errors++;
 	} else {
-		data.email = form.email.value;
+		data.datetimepicker = form.datetimepicker.value;
 	}
 
+	if (!errors && !wrongEmail) {
+		$.ajax({
+			type: 'POST',
+			url: '/profile/personal/change',
+			data: data,
+			success: function (response) {
+				response = JSON.parse(response);
 
-
-	console.log(data);
-	if (!errors)
-		console.log('ok');
+				console.log(response);
+			}
+		});
+	} else {
+		console.log('wrong email' + wrongEmail);
+	}
 });
