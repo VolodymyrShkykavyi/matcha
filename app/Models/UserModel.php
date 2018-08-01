b2fe74bac0552f299278f9fa2f084b866eb7d2bc
VolodymyrShkykavyi <volodymyr.shkykavyi@gmail.com>
2018-08-01 16:47:15 +0300

    rating calculation

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -84,6 +84,52 @@ class UserModel extends Model
 		return $res;
 	}
 
+	public function countFriends($userId)
+	{
+		$res = $this->db->query(
+			"SELECT COUNT(*) FROM `friends` WHERE `status` = 1 AND (`from_request` = ?i OR `to_request` = ?i)",
+			$userId, $userId);
+
+		$res = $res->fetch_row();
+
+		if (!empty($res))
+			return $res[0];
+		return $res;
+	}
+
+	public function countTags($userId)
+	{
+		$res = $this->db->query("SELECT COUNT(*) FROM `user_tags` WHERE `id_user` = ?i", $userId);
+
+		$res = $res->fetch_row();
+
+		if (!empty($res))
+			return $res[0];
+		return $res;
+	}
+
+	public function countOpenReports($userId)
+	{
+		$res = $this->db->query("SELECT COUNT(*) FROM `user_reports` WHERE `id_user` = ?i AND checked = 0", $userId);
+
+		$res = $res->fetch_row();
+
+		if (!empty($res))
+			return $res[0];
+		return $res;
+	}
+
+	public function countUnicVisitors($userId)
+	{
+		$res = $this->db->query("SELECT COUNT(*) FROM `notifications` WHERE `id_user` = ?i AND type = 'view_profile'", $userId);
+
+		$res = $res->fetch_row();
+
+		if (!empty($res))
+			return $res[0];
+		return $res;
+	}
+
 	public function getUserLocation($userId)
 	{
 		if (!is_numeric($userId) || $userId <= 0) {
