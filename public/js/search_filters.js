5b554f5102e289904d267a3900851db282d77d37
VolodymyrShkykavyi <volodymyr.shkykavyi@gmail.com>
2018-08-15 13:57:23 +0300

    load page, max 20 result send

--- public/js/search_filters.js
+++ public/js/search_filters.js
@@ -31,6 +31,10 @@ $("#filter_add_tag").click(function(){
 	}
 });
 
+$(document).ready(function(){
+	getResults();
+});
+
 function delTag(el){
 	let index = tags.indexOf(el.innerText.substr(1));
 
@@ -65,24 +69,6 @@ function getResults(){
 		success: function(response){
 			response = JSON.parse(response);
 
-			$(search_list).empty();
-
-			for (let el in response){
-
-				let new_el = "<li>login: " + response[el].login +
-					"<br>age: " + response[el].age + 
-					"<br>rating: " + response[el].rating;
-				if (response[el].num_shared_tags){
-					new_el += "<br>number of shared tags:" + response[el].num_shared_tags;	
-				}
-				new_el += "</li>";
-				$(search_list).append(new_el);
-					
-				//console.log(response[el]);
-			}
-			var i = 0;
-
-			console.log(response);
 			$("#Search_res").html(" ");
 			for (let user in response)
 			{
@@ -95,10 +81,15 @@ function getResults(){
 					cast_status  = '<div class="country">' + response[user].status + '</div>'; 
 				else
 					cast_status = "";
-				var res = '<div class="col-xl-5 col-lg-6 col-md-6 col-sm-6 col-xs-6"><div class="ui-block" data-mh="friend-groups-item"><div class="friend-item friend-groups"><div class="friend-item-content"><div class="more"><svg class="olymp-three-dots-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-three-dots-icon"></use>	</svg><ul class="more-dropdown">' + adm + '</ul></div><div class="friend-avatar"><div class="author-thumb"><img src="/img/' + response[user].img + '"></div><div class="author-content"><a href="/profile/' +  response[user].id + '" class="h5 author-name">' + response[user].login + '</a>' + cast_status + '<div class="country">age: ' + response[user].age + '</div><div class="country">rating: ' + response[user].rating + '<i class="fa fa-star" style="color: #f5c310;"></i></div></div></div>	<div class="control-block-button"><a data-type="add" data-id="' +  response[user].id + '" title="Send request" href="#" class="btn btn-control bg-green friend" data-toggle="modal" data-target="#create-friend-group-add-friends"><svg class="olymp-happy-faces-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-happy-faces-icon"></use>	</svg></a></div></div></div></div></div>';
+				let res = '<div class="col-6">' + 
+					'<div class="ui-block" data-mh="friend-groups-item"><div class="friend-item friend-groups"><div class="friend-item-content"><div class="more"><svg class="olymp-three-dots-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-three-dots-icon"></use>	</svg><ul class="more-dropdown">' + adm + '</ul></div><div class="friend-avatar"><div class="author-thumb"><img src="/img/' + response[user].img + '"></div><div class="author-content"><a href="/profile/' +  response[user].id + '" class="h5 author-name">' + response[user].login + '</a>' + cast_status + '<div class="country">age: ' + response[user].age + '</div><div class="country">rating: ' + response[user].rating + '<i class="fa fa-star" style="color: #f5c310;"></i></div></div></div>	<div class="control-block-button" onclick="add_friend(this);"><a data-type="add" data-id="' +  response[user].id + '" title="Send request" href="#" class="btn btn-control bg-green friend" data-toggle="modal"><svg class="olymp-happy-faces-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-happy-faces-icon"></use>	</svg></a></div></div></div></div></div>';
 				
 				$("#Search_res").html($("#Search_res").html() + res);
 			}
 		}
 	});
+}
+
+function add_friend(el){
+	console.log(el);
 }
